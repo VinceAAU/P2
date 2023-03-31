597dd32f8b4bde21d7e08392e70205287a2bd1d7
Vincent Kosteyev Bechmann <vbechm22@student.aau.dk>
2023-03-31 11:27:37 +0200

    Created basic skeleton for project
    Obviously, more files will (hopefully) be added. May the odds be ever
    in your favour.

---
+++ master/main.js
@@ -0,0 +1,5 @@
+/**
+ * This is the main entry point for the master server software. It can 
+ * (ideally) be run with `node main.js`, and will then set up a web 
+ * server
+ */
---
+++ worker/main.js
@@ -0,0 +1,4 @@
+/**
+ * The worker module is entirely web-based, and can simply be opened 
+ * in the browser.
+ */
