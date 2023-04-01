d979640ce8bd89ebadb9e8f919360f6be262d96f
Dani <danbodker@gmail.com>
2023-04-01 16:03:37 +0200

    Added basic start button

--- worker/main.js
+++ worker/main.js
@@ -2,3 +2,12 @@
  * The worker module is entirely web-based, and can simply be opened 
  * in the browser!
  */
+
+function toggleStartButton() {
+    var button = document.getElementById("startButton");
+    if (button.innerHTML === "Start") {
+      button.innerHTML = "Disconnect";
+    } else {
+      button.innerHTML = "Start";
+    }
+  }
