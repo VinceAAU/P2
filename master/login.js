3a8155ac5019d7b31e1c5716080d46873199b896
Chris <cgamel22@student.aau.dk>
2023-04-03 07:52:48 +0200

    Faulty json reading

--- master/login.js
+++ master/login.js
@@ -1,17 +1,36 @@
 export {user_login_info};
 import {fileResponse, requestHandler} from "./server.js"
 
+//important fs
+import fs from 'fs'
+
+
+
+
 const ValidationError="Validation Error";
 const UserNotRecognized="User not recognized";
 const path="P2/worker/login.html";
 
+
+
 function user_login_info(user_info){
-    console.log(user_info['userName']);
-    let userExists = true;
-    if(userExists == true){
-      console.log("user exists");
-      return "success";
+
+  console.log("Supposed to read Json --------------");
+  fs.readFile('./users.json', 'utf-8', (err, jsonString) => {
+    if(err) {
+      console.log(err);
+    } else {
+    console.log(jsonString);
     }
-    console.log("no user");
-    throw(path);
+  });
+  console.log("Supposed to  stop reading read Json --------------");
+
+  console.log(user_info['userName']);
+  let userExists = true;
+  if(userExists == true){
+    console.log("user exists");
+    return "success";
+  }
+  console.log("no user");
+  throw(path);
   }
