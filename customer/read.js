d1e3386a38d0d56785b767036f94f074f2a58283
Phutsorn <AM55QK@student.aau.dk>
2023-04-05 00:55:08 +0200

    Stortset kopiret fra nettet, men har fået costumer page til nogenlunde at virke

--- customer/read.js
+++ customer/read.js
@@ -1,5 +1,6 @@
 const fileInput = document.querySelector('input');
 const preview = document.getElementById('preview');
+let csv;
 
 
 //Saves a csv file in arrays and splits it up by comma
@@ -10,10 +11,30 @@ fileInput.addEventListener('change', () => {
     fr.addEventListener('load', () => {
         const csv = fr.result;
 
-        const array = csv.split('\r\n').map((line) => {
+        array = csv.split('\r\n').map((line) => {
             return line.split(',');
         });
 
         console.log(array);
     })
 })
+
+function download() {
+    const downloadLink = document.createElement('a');
+
+    // Create the CSV file as a Blob
+    const csvBlob = new Blob([array.join('\n')], { type: 'text/csv' });
+
+    // Create a URL for the Blob
+    const csvUrl = URL.createObjectURL(csvBlob);
+
+    // Set the download link href to the URL
+    downloadLink.href = csvUrl;
+
+    // Set the download link download attribute to the filename
+    downloadLink.download = fileInput.files[0].name;
+
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+}
