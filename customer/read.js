81e0ec4ba4bef63390ca705950e417be5b40f1d1
Phutsorn <AM55QK@student.aau.dk>
2023-04-09 23:20:35 +0200

    Dynamic costumer page

--- customer/read.js
+++ customer/read.js
@@ -1,39 +1,56 @@
 const fileInput = document.querySelector('input');
 const preview = document.getElementById('preview');
-let csv;
+let arrays = [];
+let files = []; // Array to store uploaded files
 
-//Saves a csv file in arrays and splits it up by comma
+// Saves a csv file in arrays and splits it up by comma
 fileInput.addEventListener('change', () => {
     const fr = new FileReader();
-    fr.readAsText(fileInput.files[0]);
-
-    fr.addEventListener('load', () => {
+    const uploadedFiles = fileInput.files; // Store uploaded files in a variable
+  
+    for (let i = 0; i < uploadedFiles.length; i++) {
+      const file = uploadedFiles[i];
+  
+      fr.readAsText(file);
+  
+      fr.addEventListener('load', () => {
         const csv = fr.result;
-
-        array = csv.split('\r\n').map((line) => {
-            return line.split(',');
+  
+        const array = csv.split('\r\n').map((line) => {
+          return line.split(',');
         });
+        arrays.push(array);
+        files.push(file); // Store the uploaded file in the files array
+  
+        document.getElementById('numFiles').textContent = `Number of files uploaded ${arrays.length}`;
+  
+        const downloadBtn = document.createElement('button');
+        downloadBtn.textContent = `Download File ${arrays.length}`;
+        downloadBtn.addEventListener('click', (event) => {
+          const index = parseInt(event.target.textContent.split(' ')[2]) - 1;
+          download(index);
+        });
+  
+        document.body.appendChild(downloadBtn);
+      });
+    }
+  });
 
-        console.log(array);
-    })
-})
-
-function download() {
-    const downloadLink = document.createElement('a');
-
-    // Create the CSV file as a Blob
-    const csvBlob = new Blob([array.join('\n')], { type: 'text/csv' });
-
-    // Create a URL for the Blob
-    const csvUrl = URL.createObjectURL(csvBlob);
-
-    // Set the download link href to the URL
-    downloadLink.href = csvUrl;
-
-    // Set the download link download attribute to the filename
-    downloadLink.download = fileInput.files[0].name;
-
-    document.body.appendChild(downloadLink);
-    downloadLink.click();
-    document.body.removeChild(downloadLink);
-} 
+function download(index) {
+    if (arrays.length > index && files.length > index) {
+      const downloadLink = document.createElement('a');
+  
+      const csvBlob = new Blob([arrays[index].map(line => line.join(',')).join('\n')], { type: 'text/csv' });
+  
+      const csvUrl = URL.createObjectURL(csvBlob);
+  
+      downloadLink.href = csvUrl;
+      downloadLink.download = files[index].name;
+  
+      document.body.appendChild(downloadLink);
+      downloadLink.click();
+      document.body.removeChild(downloadLink);
+    } else {
+      console.error(`File not found for index ${index}`);
+    }
+  }
