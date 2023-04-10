ff99fdd1ca0dd0ed8660170fd23b8b22a52ad87b
Dani <danbodker@gmail.com>
2023-04-10 19:14:30 +0200

    fixed server crash if given incorrect path

--- master/send_data.js
+++ master/send_data.js
@@ -1,14 +1,16 @@
 import fs from "fs";
 
-export function start_data_stream(path, res)
-{
-    try{
-        const readStream = fs.createReadStream(path); 
-        readStream.pipe(res);
-        console.log("Begun streaming data");
-    }
-    catch (e)
-    {
-        console.log("Couldn't start datastream. Probably because the filepath is wrong");
-    }
+export function start_data_stream(path, res) {
+
+    fs.access(path, (err) => { // makes sure the server doesn't crash, if given an incorrect path
+        if (!err) {
+            const readStream = fs.createReadStream(path);
+            readStream.pipe(res);
+            console.log("Begun streaming data");
+        } else {
+            
+            console.error(`${path} does not exist, or is not accessible`);
+        }
+    });
+
 }
