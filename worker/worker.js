d98cac621ccfee75660336a7fcc0f77d6347672c
Viktor Dahl Johnsen <112980916+vjohns22@users.noreply.github.com>
2023-04-13 21:53:07 +0200

    Added QuickSort. A lot of comments too.

--- worker/worker.js
+++ worker/worker.js
@@ -1,30 +1,108 @@
 /*  The worker takes an array and performs QuickSort on it; 
-    returns the sorted array.                                         */
+    returns the array, sorted.                               */
 
 onmessage = function(e) {
   console.log("Worker: received block of work. ");
   console.log("Unsorted: " + e.data);
 
-  //  Trying out QuickSort.
-  function quicksort(array) {
-    if (array.length <= 1) {
-      return array;
+  let amountOfRecursions = 0; //  Keeping track of how many times QuickSort is called, for science!
+
+  /**
+   * Implementation of the QuickSort algorithm. Sorts in-place and uses a random pivot.
+   * @param {*} arr The array to sort, in-place.
+   * @param {*} left The left-most index.
+   * @param {*} right The right-most index.
+   * @returns The function does not return anything as it sorts the array in-place.
+   */
+  function quickSort(arr, left = 0, right = arr.length - 1) {
+
+    //  Base case: array is already sorted (~length less than two).
+    if (left >= right) {
+      return; //  Stops the recursive call-chain.
     }
-  
-    let pivot = array[0];
     
-    let left = []; 
-    let right = [];
+    //  Choosing a random pivot to decrease likelihood of worst-case running time.
+    const pivotIndex = getRandomInt(left, right);
+    const pivot = arr[pivotIndex];
+
+    //  ------------------------ For debugging. ------------------------
+    console.log(`Pivot for run #${amountOfRecursions} is ${pivotIndex}`); 
+    amountOfRecursions++;
+
+    //  Partition the array into two subarrays.
+    const partitionIndex = partition(arr, pivot, left, right);
   
-    for (let i = 1; i < array.length; i++) {
-      array[i] < pivot ? left.push(array[i]) : right.push(array[i]);
+    //  Recursively sort the subarrays.
+    quickSort(arr, left, partitionIndex - 1);
+    quickSort(arr, partitionIndex, right);
+  }
+  
+  /**
+   * The partition part of QuickSort. It places the pivot at the correct position of the array; 
+   * smaller elements are placed before it and larger elements are placed after it in the array.
+   * @param {*} arr 
+   * @param {*} pivot 
+   * @param {*} left Renamed 'i' in the code. This is one of two pointers used to scan through the array left to right and right to left.
+   * @param {*} right Renamed 'j' in the code. This is the other pointer.
+   * @returns The index of the partition point.
+   */
+  function partition(arr, pivot, left, right) {
+
+    //  Move elements smaller than pivot to the left, and elements larger than the pivot to the right.
+    let i = left;
+    let j = right;
+  
+    while (i <= j) {
+      //  Next element from the left that - is greater than or equal to pivot.
+      while (arr[i] < pivot) {
+        i++;
+      }
+  
+      //  Next element from the right - that is less than or equal to pivot.
+      while (arr[j] > pivot) {
+        j--;
+      }
+  
+      //  Swap the elements if they are in the wrong order.
+      if (i <= j) {
+        swap(arr, i, j);
+        i++;
+        j--;
+      }
     }
   
-    return quicksort(left).concat(pivot, quicksort(right));
-  };
-  let resultArr = new Int32Array(quicksort(e.data));
+    //  Return the index of the partition point.
+    return i;
+  }
+  
+  /**
+   * Getting a random integer from the specific range (in this case, defined by the left and right indices).
+   * @param {*} min The lower bound of the range.
+   * @param {*} max The upper bound of the range.
+   * @returns A random integer from the range (min and max included).
+   */
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  
+  /**
+   * Swaps element indices. Note: In-place; it manipulates the given array, thusly, it does not return anything.
+   * @param {*} arr An array that needs elements swapped.
+   * @param {*} i One element, that needs to be switched with another element.
+   * @param {*} j --||--
+   */
+  function swap(arr, i, j) {
+    const temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+  
+  //  Running QuickSort, in-place on the provided array.
+  quickSort(e.data);
 
-  // Experimenting with Transferrable objects.
-  postMessage(resultArr, [resultArr.buffer]);
+  //  Returning the now-sorted array using POST.
+  this.postMessage(e.data, [e.data.buffer]);
 
+  //  Logging the amount of times QuickSort was called, for fun (also somewhat informative).
+  console.log(`QuickSort ran ${amountOfRecursions} recursive calls. `);
 }
