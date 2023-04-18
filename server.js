7faf4f55a7e097c3bbbfdf8f970cb581a9b1b215
Chris <christoffer.gamel@gmail.com>
2023-04-18 16:25:42 +0200

    fix for windows

--- server.js
+++ server.js
@@ -5,12 +5,12 @@
  */
 
 const loginPath = '/worker/html/login.html';
-const workerPath = 'worker/html/workerPage.html';
-const createUserPath = 'worker/html/createUser.html';
-const forgotPasswordPath = 'worker/html/forgotPassword.html';
-const changePasswordPath = 'worker/html/changePassword.html';
-const indexPath = 'worker/html/index.html';
-const customerPagePath = 'customer/customerPage.html';
+const workerPath = '/worker/html/workerPage.html';
+const createUserPath = '/worker/html/createUser.html';
+const forgotPasswordPath = '/worker/html/forgotPassword.html';
+const changePasswordPath = '/worker/html/changePassword.html';
+const indexPath = '/worker/html/index.html';
+const customerPagePath = '/customer/customerPage.html';
 const CSSPath = 'worker/style.css';
 
 
@@ -77,6 +77,7 @@ function requestHandler(req, res) {
       streamArray(res, [5, 2, 1, 2, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6]); // Example array
       break;
     case "/worker/html/posts":
+      console.log("posts")
       authenticateToken(req, res);
       break;
 
@@ -194,6 +195,7 @@ function returnToken(req, res, username) {
 }
 
 function authenticateToken(req, res, next) {
+  console.log("authenticate token function")
   const str = '473f2eb9c7b9a92b59f2990e4e405fedb998dd88a361c0a8534c6c9988a44fa5eeeb5aea776de5b45bdc3cabbc92a8e4c1074d359aacba446119e82f631262f0'; //to be put in .env
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
@@ -207,7 +209,10 @@ function authenticateToken(req, res, next) {
 
   jwt.verify(token, str, (err, user) => {
     if (err) return errorResponse(res, 403, err)
-    req.user = user;
+    req.user = 'admin';
+    console.log("token authenticated")
+    res.statusCode = 200;
+    res.end("\n");
   });
 };
 
@@ -293,6 +298,7 @@ function guessMimeType(fileName) {
 }
 
 function errorResponse(res, code, reason) {
+  console.log("error response")
   res.statusCode = code;
   res.setHeader('Content-Type', 'text/txt');
   res.write(reason);
--- worker/loginClient.js
+++ worker/loginClient.js
@@ -8,53 +8,56 @@ function checkboxClick(event) {
     toggleLogin(event)
 }
 
-async function toggleLogin(event) { // async await 
-    console.log("funcS");
-    const button = document.querySelector("#submitBtn_id");
+async function toggleLogin(event) {
+  console.log("funcS");
+  const button = document.querySelector("#submitBtn_id");
 
-    var username = document.getElementById("name_id");
-    var password = document.getElementById("password_id");
-    //console.log(username['value'] + password['value'])
+  var username = document.getElementById("name_id");
+  var password = document.getElementById("password_id");
 
-    const bodyData = new URLSearchParams();
-    bodyData.append('username', username['value']);
-    bodyData.append('password', password['value']);
+  const bodyData = new URLSearchParams();
+  bodyData.append('username', username['value']);
+  bodyData.append('password', password['value']);
 
-    fetch('/worker/html/fetchUser', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: bodyData
-      })
-        .then((response) => {
-          if (!response.ok) {
-            throwError()
-          }
-          return response.json();
-        })
-        .then((data) => {
-          console.log(data);
-          returnToken(data)
-            .then((response) => {
-              if (response.ok) {
-                window.location.href = '/loggedIn';// getCache();//'/page.html';
-              } else {
-                console.log('Response not ok');
-              }
-            })
-            .catch((error) => console.log(error));
-        })
-        .catch((error) => console.error(error));
+  try {
+    const response = await fetch('/worker/html/fetchUser', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: bodyData
+    });
+
+    if (!response.ok) {
+      throwError();
+    }
+
+    const data = await response.json();
+    console.log(data);
+
+    const tokenResponse = await returnToken(data);
+
+    console.log(tokenResponse);
+
+    if (tokenResponse.ok) {
+      console.log("response OK")
+      window.location.href = '/loggedIn';// getCache();//'/page.html';
+    } else {
+      console.log('Response not ok');
+    }
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 function returnToken(data) {
-    const requestOptions = {
-        method: 'GET',
-        headers: { 'Authorization': `Bearer ${data.accessToken}` }
-    };
+  console.log("return token function")
+  const requestOptions = {
+    method: 'GET',
+    headers: { 'Authorization': `Bearer ${data.accessToken}` }
+  };
 
-    return fetch('http://localhost:3000/worker/html/posts', requestOptions)
+  return fetch('http://localhost:3000/worker/html/posts', requestOptions);
 }
 
 function throwError() {
