43ea8d7088f48b615b6232798f8aa25805b5681e
Dani <danbodker@gmail.com>
2023-05-21 15:45:14 +0200

    fixed fetch

--- worker/clientsideJS/forgotPasswordClient.js
+++ worker/clientsideJS/forgotPasswordClient.js
@@ -13,7 +13,7 @@ async function toggleSearch(event) {
     bodyData.append('username', username['value']);
 
     try {
-        const response = await fetch('/forgot-password-post', {
+        const response = await fetch('forgot-password-post', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded'
