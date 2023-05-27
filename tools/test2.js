b63c178a8319f65a084843db24f594a31271e333
Dani <danbodker@gmail.com>
2023-05-27 02:38:43 +0200

    stuff

--- tools/test2.js
+++ tools/test2.js
@@ -3,62 +3,57 @@ import fs from "fs";
 
 let buckets = [];
 
-for(let i = 0; i < 10; i++)
-{
-    buckets.push(Uint32Array.from({length: 50000}, () => Math.floor(Math.random() * 10000000)));
+for (let i = 0; i < 10; i++) {
+    buckets.push(Uint32Array.from({ length: 50000 }, () => Math.floor(Math.random() * 10000000)));
 }
 //let inputArray = Uint32Array.from({length: 5000000}, () => Math.floor(Math.random() * 10000000));
 console.log("finished making arrays");
 
 console.log(buckets);
 
-function getCharsForCSV(inputArray)
-{
+function getCharsForCSV(inputArray) {
     let numberOfDigits = 0;
-    for(let i = 0; i < inputArray.length; i ++) { // goes through every element, counts the elements digits and sums them all together
+    for (let i = 0; i < inputArray.length; i++) { // goes through every element, counts the elements digits and sums them all together
         numberOfDigits += Math.log(inputArray[i]) * Math.LOG10E + 1 | 0;
     }
     numberOfDigits += inputArray.length; // one comma for each number, and the last number has a newline, which is the same length
     return numberOfDigits;
 }
 
-function getBucketFileOffsets(allBuckets)
-{
+function getBucketFileOffsets(allBuckets) {
     let offsets = []
     let currentOffset = 0;
 
     offsets[0] = 0; // first bucket should obviously be placed at the start of the file.
-    for(let i = 1; i < allBuckets.length; i++)
-    {
-        currentOffset += getCharsForCSV(allBuckets[i-1]);
+    for (let i = 1; i < allBuckets.length; i++) {
+        currentOffset += getCharsForCSV(allBuckets[i - 1]);
         offsets[i] = currentOffset;
     }
 
     return offsets;
 }
 
-function writeBucketToFile(offset, bucket, filePath)
-{
+function writeBucketToFile(offset, bucket, filePath) {
     console.log("Opening file!!!");
-        fs.open(filePath, 'r+', function(err, fd) {
+    fs.open(filePath, 'r+', (err, fd) => {
         if (err) throw err;
-      
-          const csvString = bucket.join(',') + '\n';
-          const buffer = Buffer.from(csvString);
-          fs.write(fd, buffer, 0, buffer.length, offset, function(err) {
+
+        const csvString = bucket.join(',') + '\n';
+        const buffer = Buffer.from(csvString);
+        fs.write(fd, buffer, 0, buffer.length, offset, (err) => {
             if (err) throw err;
-          });
-        console.log("Closing file!!!");
-          fs.close(fd, function(err) {
-            if (err) throw err;
-          });
-      
-      });
+
+            console.log("Closing file!!!");
+            fs.close(fd, (err) => {
+                if (err) throw err;
+            });
+        });
+    });
 }
+
 let offsets = getBucketFileOffsets(buckets);
 console.log(offsets);
-for(let i = 0; i < 10; i++)
-{
+for (let i = 0; i < 10; i++) {
     writeBucketToFile(offsets[i], buckets[i], "file.csv");
 }
 
